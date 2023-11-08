7e8da81fd20a5959f4dc4ec008dbbc587e7e1676
Cale Jones <CJones@LickingCounty.gov>
2023-11-08 13:58:35 -0500

    Update fibonacci.js

--- js/fibonacci.js
+++ js/fibonacci.js
@@ -5,37 +5,35 @@ document.addEventListener("DOMContentLoaded", function () {
   const fibonacciColors = ["#ff5733", "#33ff57", "#5733ff", "#ff3357", "#57ff33"];
 
   function drawFibonacciSpiral(n) {
-    console.log("drawing...");
-    let a = 0;
-    let b = 1;
-    let temp;
-    let angle = 0;
-    const scale = 10;
+  console.log("drawing...");
+  let a = 0;
+  let b = 1;
+  let temp;
+  const scale = 10;
+  let x = 200; // Adjust these based on your container size
+  let y = 200; // Adjust these based on your container size
+  let angle = 0;
 
-    for (let i = 0; i < n; i++) {
-      console.log(i);
-      context.beginPath();
-      context.strokeStyle = fibonacciColors[i % fibonacciColors.length];
-      context.lineWidth = 2;
+  for (let i = 0; i < n; i++) {
+    console.log(i);
+    context.beginPath();
+    context.strokeStyle = fibonacciColors[i % fibonacciColors.length];
+    context.lineWidth = 2;
 
-      // Calculate the next Fibonacci number
-      const c = a + b;
-      a = b;
-      b = c;
+    // Draw a line
+    context.moveTo(x, y);
+    x += scale * Math.cos(angle);
+    y += scale * Math.sin(angle);
+    context.lineTo(x, y);
+    context.stroke();
 
-      // Calculate the position
-      const x = scale * Math.cos(angle);
-      const y = scale * Math.sin(angle);
+    // Calculate the next Fibonacci number
+    const c = a + b;
+    a = b;
+    b = c;
 
-      // Draw a line
-      context.moveTo(200, 200); // Adjust this based on your container size
-      context.lineTo(200 + x, 200 + y); // Adjust this based on your container size
-      context.stroke();
-
-      // Update the angle for the next iteration
-      angle += Math.PI / 2;
-    }
+    // Update the angle for the next iteration
+    angle += Math.PI / 2;
   }
+}
 
-  drawFibonacciSpiral(10); // Adjust the number of iterations as needed
-});
