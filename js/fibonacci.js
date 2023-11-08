d299ddaa54cbb4fd8ba145cb1cc7e6ed5f1f0ddc
Cale Jones <CJones@LickingCounty.gov>
2023-11-08 14:41:11 -0500

    Update fibonacci.js

--- js/fibonacci.js
+++ js/fibonacci.js
@@ -3,49 +3,45 @@ document.addEventListener("DOMContentLoaded", function () {
   const context = container.getContext("2d");
 
   const fibonacciColors = ["#ff5733", "#33ff57", "#5733ff", "#ff3357", "#57ff33"];
+  const scale = 10;
+  let a = 0;
+  let b = 1;
+  let temp;
+  let x = 200; // Adjust these based on your container size
+  let y = 200; // Adjust these based on your container size
+  let angle = 0;
+  let iterations = 10; // Adjust the number of iterations
+  let currentIteration = 0;
 
-  function drawFibonacciSpiral(n, delay) {
-    console.log("drawing...");
-    let a = 0;
-    let b = 1;
-    let temp;
-    const scale = 200;
-    let x = 200; // Adjust these based on your container size
-    let y = 200; // Adjust these based on your container size
-    let angle = 0;
+  function drawLine() {
+    context.beginPath();
+    context.strokeStyle = fibonacciColors[currentIteration % fibonacciColors.length];
+    context.lineWidth = 2;
 
-    function drawNextLine() {
-      context.beginPath();
-      context.strokeStyle = fibonacciColors[n % fibonacciColors.length];
-      context.lineWidth = 2;
+    // Draw a line
+    context.moveTo(x, y);
+    x += scale * Math.cos(angle);
+    y += scale * Math.sin(angle);
+    context.lineTo(x, y);
+    context.stroke();
 
-      // Draw a line
-      context.moveTo(x, y);
-      x += scale * Math.cos(angle);
-      y += scale * Math.sin(angle);
-      context.lineTo(x, y);
-      context.stroke();
+    // Calculate the next Fibonacci number
+    const c = a + b;
+    a = b;
+    b = c;
 
-      // Calculate the next Fibonacci number
-      const c = a + b;
-      a = b;
-      b = c;
+    // Update the angle for the next iteration
+    angle += Math.PI / 2;
 
-      // Update the angle for the next iteration
-      angle += Math.PI / 2;
+    currentIteration++;
 
-      n--;
-
-      if (n > 0) {
-        setTimeout(drawNextLine, delay);
-      } else {
-        console.log("Finished drawing.");
-      }
+    if (currentIteration < iterations) {
+      requestAnimationFrame(drawLine); // Continue to the next iteration
+    } else {
+      console.log("Finished drawing.");
     }
-
-    drawNextLine();
   }
 
-  // Adjust the number of iterations and delay as needed
-  drawFibonacciSpiral(30, 500); // 10 iterations with a 500 ms delay between lines
+  // Start the drawing
+  requestAnimationFrame(drawLine);
 });
